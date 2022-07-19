aceec44b4b15afb022957920d86f51cf4574b4c7
Epic-dev-2 <noah@Epic-dev-2s-Mac-mini.local>
2022-07-19 13:50:03 -0400

    updated stylesheets

--- App.js
+++ App.js
@@ -1,29 +1,44 @@
 //import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.testText}>This is my first React Native app!</Text>
-      <Text style={styles.testText}>Adding a second child component.</Text>
-      <Text>And another one</Text>
-      {/* <StatusBar style="auto" /> */}
-      <Button title='Tap me!' />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='your course goal!'/>
+        <Button title='Add Goal' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text >List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    flex: 1,
   },
-  testText: {
-    margin: 16, 
-    borderWidth: 2, 
-    borderColor: 'blue',
-    padding: 16,
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
   },
+  goalsContainer: {
+    flex: 5,
+  }
+  
 });
