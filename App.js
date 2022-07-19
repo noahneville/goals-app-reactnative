7219195e5873ec1cab4b93b03466e1fd5c1f8047
Epic-dev-2 <noah@Epic-dev-2s-Mac-mini.local>
2022-07-19 15:32:09 -0400

    functionality to input goals and add them to the list implemented

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   };
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals, 
+      enteredGoalText,
+    ]);
   };
 
   return (
@@ -27,7 +31,7 @@ export default function App() {
         />
       </View>
       <View style={styles.goalsContainer}>
-        <Text >List of goals...</Text>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
